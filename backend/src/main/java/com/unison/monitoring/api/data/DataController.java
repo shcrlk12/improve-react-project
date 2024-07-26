14361cc947e26be91ae428c308344accca4da4c5
kimjeongwon <kjwon@unison.co.kr>
2024-07-26 16:08:39 +0900

    테이블 설정 함

--- backend/src/main/java/com/unison/monitoring/api/data/DataController.java
+++ backend/src/main/java/com/unison/monitoring/api/data/DataController.java
@@ -1,4 +1,11 @@
 package com.unison.monitoring.api.data;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/data")
+@RequiredArgsConstructor
 public class DataController {
 }
---
+++ backend/src/main/java/com/unison/monitoring/api/generalOverview/GeneralOverviewEntity.java
@@ -0,0 +1,47 @@
+package com.unison.monitoring.api.generalOverview;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "GeneralOverview")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+public class GeneralOverviewEntity {
+    @Id
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String siteName;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = false)
+    @ColumnDefault("1")
+    private boolean isActive;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private boolean isDelete;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @ColumnDefault("'System'")
+    private String createdBy;
+}
